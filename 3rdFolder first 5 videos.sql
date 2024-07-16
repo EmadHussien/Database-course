a77583d6cfd6d2e030eea1f4126ac20e7707463f
Emad Hussien <emadhussien851@gmail.com>
2024-07-16 18:37:26 +0300

    Update 3rdFolder first 5 videos.sql

--- 3rdFolder first 5 videos.sql
+++ 3rdFolder first 5 videos.sql
@@ -1,24 +1,21 @@
+-- Select 'order_id', 'product_id', 'name', 'quantity', and 'unit_price' from the 'order_items' and 'products' tables.
+-- Join the 'order_items' table (OI) with the 'products' table (P) on the 'product_id' column.
+SELECT order_id, 
+       P.product_id, 
+       P.name, 
+       quantity, 
+       P.unit_price
+FROM order_items OI
+JOIN products P ON OI.product_id = P.product_id;
 
-/*
-SELECT ORDER_ID , P.PRODUCT_ID , P.NAME ,quantity, P.unit_price  
-	FROM 
-	order_items OI 
-    JOIN 
-    PRODUCTS P
-	ON OI.product_id = P.PRODUCT_ID;
-    
-    */
-    
-    SELECT 
-    P.PAYMENT_ID,
-    P.INVOICE_ID,
-    P.AMOUNT,
-    C.NAME,
-    PM.NAME
-    AS "PAYMENT METHOD"
-    FROM PAYMENTS P 
-    JOIN clients C 
-		ON P.CLIENT_ID = C.client_id
-	JOIN payment_methods PM 
-		ON P.PAYMENT_METHOD = PM.payment_method_id;
-    
+-- Select 'payment_id', 'invoice_id', 'amount', client's 'name', and 'payment method' name from the 'payments', 'clients', and 'payment_methods' tables.
+-- Join the 'payments' table (P) with the 'clients' table (C) on 'client_id'.
+-- Join the 'payments' table (P) with the 'payment_methods' table (PM) on 'payment_method_id'.
+SELECT P.payment_id,
+       P.invoice_id,
+       P.amount,
+       C.name,
+       PM.name AS "payment method"
+FROM payments P
+JOIN clients C ON P.client_id = C.client_id
+JOIN payment_methods PM ON P.payment_method = PM.payment_method_id;
