a9d8484044efa209ad8d62aa6868e2c0bbd232bf
Emad Hussien <emadhussien851@gmail.com>
2024-07-16 18:40:32 +0300

    Update first 5 videos in folder4.sql

--- first 5 videos in folder4.sql
+++ first 5 videos in folder4.sql
@@ -1,19 +1,24 @@
-/*
-INSERT INTO PRODUCTS 
-(NAME,QUANTITY_IN_STOCK,UNIT_PRICE)
-VALUES("BAG" , 5 , 17),
-		("SHAVING MACHINES", 50,10),
-        ("NAPKINS",60,18);
-        
-        
--- INVOICES_ARCHIVE
-CREATE TABLE INVOICES_ARCHIVE AS
-SELECT I.INVOICE_ID , I.NUMBER,C.NAME,I.INVOICE_TOTAL,I.PAYMENT_TOTAL,I.INVOICE_DATE,I.DUE_DATE,
- I.PAYMENT_DATE
- FROM
-INVOICES I
-JOIN 
-CLIENTS C
-USING(CLIENT_ID)
-WHERE I.PAYMENT_DATE IS NOT NULL;
-*/
+-- Insert new rows into the 'products' table with specified values for 'name', 'quantity_in_stock', and 'unit_price'.
+-- The values being inserted are ("BAG", 5, 17), ("SHAVING MACHINES", 50, 10), and ("NAPKINS", 60, 18).
+INSERT INTO products 
+(name, quantity_in_stock, unit_price)
+VALUES ("BAG", 5, 17),
+       ("SHAVING MACHINES", 50, 10),
+       ("NAPKINS", 60, 18);
+
+-- Create a new table 'invoices_archive' and populate it with selected columns from the 'invoices' and 'clients' tables.
+-- The new table includes 'invoice_id', 'number', client's 'name', 'invoice_total', 'payment_total', 'invoice_date', 'due_date', and 'payment_date'.
+-- Join the 'invoices' table (I) with the 'clients' table (C) using 'client_id'.
+-- Only include rows where 'payment_date' is not null (invoices that have been paid).
+CREATE TABLE invoices_archive AS
+SELECT I.invoice_id, 
+       I.number, 
+       C.name, 
+       I.invoice_total, 
+       I.payment_total, 
+       I.invoice_date, 
+       I.due_date, 
+       I.payment_date
+FROM invoices I
+JOIN clients C USING (client_id)
+WHERE I.payment_date IS NOT NULL;
