ecbb01de46eaf6e07c15570083bc9b6d96acc964
Emad Hussien <emadhussien851@gmail.com>
2024-07-16 18:39:08 +0300

    Update FINISHING THE 3RD FOLDER.sql

--- FINISHING THE 3RD FOLDER.sql
+++ FINISHING THE 3RD FOLDER.sql
@@ -1,24 +1,45 @@
-/*
-SELECT SH.NAME , P.NAME
- FROM SHIPPERS SH 
-  cross JOIN PRODUCTS P
-	ORDER BY P.NAME;
+-- Select the 'name' from the 'shippers' table and the 'name' from the 'products' table.
+-- Perform a cross join between 'shippers' (SH) and 'products' (P), which returns the Cartesian product of both tables.
+-- Order the results by the 'name' of the product.
+SELECT SH.name, 
+       P.name
+FROM shippers SH
+CROSS JOIN products P
+ORDER BY P.name;
 
- 
-SELECT SH.NAME , P.NAME
- FROM SHIPPERS SH , PRODUCTS P
-	ORDER BY P.NAME;
-    */
-	SELECT customer_id,FIRST_NAME, POINTS ,  "BRONZE" AS TYPE 
-	FROM customers   
-		WHERE POINTS < 2000
-	UNION		
-	SELECT customer_id,FIRST_NAME, POINTS ,  "SILVER" AS TYPE 
-	FROM customers   
-		WHERE POINTS  between 2000 AND 3000 
-    UNION
-    SELECT customer_id,FIRST_NAME, POINTS ,  "GOLD" AS TYPE 
-	FROM customers   
-		WHERE POINTS > 3000
-    ORDER BY FIRST_NAME
-    
+-- Select the 'name' from the 'shippers' table and the 'name' from the 'products' table.
+-- Perform a cross join between 'shippers' (SH) and 'products' (P) using the old join syntax (equivalent to CROSS JOIN).
+-- Order the results by the 'name' of the product.
+SELECT SH.name, 
+       P.name
+FROM shippers SH, 
+     products P
+ORDER BY P.name;
+
+-- Select 'customer_id', 'first_name', 'points', and assign the type "BRONZE" for customers with points less than 2000.
+-- Union the results with:
+-- Select 'customer_id', 'first_name', 'points', and assign the type "SILVER" for customers with points between 2000 and 3000.
+-- Union the results with:
+-- Select 'customer_id', 'first_name', 'points', and assign the type "GOLD" for customers with points greater than 3000.
+-- Order the final results by 'first_name'.
+SELECT customer_id, 
+       first_name, 
+       points, 
+       "BRONZE" AS type 
+FROM customers   
+WHERE points < 2000
+UNION		
+SELECT customer_id, 
+       first_name, 
+       points, 
+       "SILVER" AS type 
+FROM customers   
+WHERE points BETWEEN 2000 AND 3000 
+UNION
+SELECT customer_id, 
+       first_name, 
+       points, 
+       "GOLD" AS type 
+FROM customers   
+WHERE points > 3000
+ORDER BY first_name;
